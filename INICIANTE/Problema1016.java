e7c9b071df806bb18dd76167905e9fb0184199c7
Salomão <101257752+5alomao@users.noreply.github.com>
2023-04-28 20:24:37 -0300

    Distância

---
+++ INICIANTE/Problema1016.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[] args){
+        
+        Scanner s = new Scanner (System.in);
+
+        int x = s.nextInt();
+        int tempo = x * 2;
+        
+        System.out.println(tempo + " minutos");
+        
+        
+        
+    }
+}
