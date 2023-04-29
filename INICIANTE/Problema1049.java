0f99a4fef6f6d7dab3b670e4d35a2b51b2c7df25
Salomão <101257752+5alomao@users.noreply.github.com>
2023-04-29 15:18:37 -0300

    Animal

---
+++ INICIANTE/Problema1049.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String[] args){
+        
+        Scanner sc = new Scanner (System.in);
+        
+        String a = sc.nextLine();
+        String b =  sc.nextLine();
+        String c =  sc.nextLine();
+        
+        if(a.equals("vertebrado")){
+            if(b.equals("ave")){
+                if(c.equals("carnivoro"))
+                    System.out.println("aguia");
+                else if(c.equals("onivoro"))
+                    System.out.println("pomba");
+            }else if(b.equals("mamifero")){
+                if(c.equals("herbivero"))
+                    System.out.println("vaca");
+                else if(c.equals("onivoro"))
+                    System.out.println("homem");
+            }
+        }else if(a.equals("invertebrado")){
+            if(b.equals("inseto")){
+                if(c.equals("hematofago"))
+                    System.out.println("pulga");
+                else if(c.equals("herbivoro"))
+                    System.out.println("largarta");
+            }else if(b.equals("anelideo")){
+                if(c.equals("hematofago"))
+                    System.out.println("sanguessuga");
+                else if(c.equals("onivoro"))
+                    System.out.println("minhoca");
+            }
+        }
+        
+    }
+}
