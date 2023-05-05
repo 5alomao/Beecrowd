bef0ec2ed3b36d103914cf38cc8cc81df28b4be2
Salomão <101257752+5alomao@users.noreply.github.com>
2023-05-05 14:26:17 -0300

    Fibonacci Fácil

---
+++ INICIANTE/Problema1151.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a = 0, b = 1;
+        System.out.print(a + " " + b);
+        
+        for (int i = 2; i < n; i++) {
+            int c = a + b;
+            System.out.print(" " + c);
+            a = b;
+            b = c;
+        }
+        System.out.println();
+    }
+}
