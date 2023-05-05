4d64743d567c181b34f711f40a03d4aa2b71871f
Salomão <101257752+5alomao@users.noreply.github.com>
2023-05-05 10:22:15 -0300

    Sequência S

---
+++ INICIANTE/Problema1155.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+	    Scanner sc = new Scanner(System.in);
+	    
+	    double S=0;
+	    
+	    for(int i=1;i<=100;i++){
+	        S += 1.0/i;
+	        
+	    }
+	    
+	    System.out.printf("%.2f\n",S);
+	}
+}
