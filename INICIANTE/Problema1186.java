87f14fd453b277f8b6cd542d47fa44c5974f8fbb
Salomão <101257752+5alomao@users.noreply.github.com>
2023-05-12 10:16:42 -0300

    Abaixo da Diagonal Secundária

---
+++ INICIANTE/Problema1186.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        char op = sc.next().charAt(0);
+
+        double[][] matriz = new double[12][12];
+        double somaAcimaDiagonal = 0.0;
+        int qtd = 0;
+        
+        sc.nextLine();
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
+                matriz[i][j] = sc.nextDouble();
+                if (i + j > 11) {
+                    somaAcimaDiagonal += matriz[i][j];
+                    qtd++;
+                }
+            }
+        }
+
+        if (op == 'M') {
+            double mediaAcimaDiagonal = somaAcimaDiagonal / qtd;
+            System.out.printf("%.1f\n", mediaAcimaDiagonal);
+        } else if (op == 'S') {
+            System.out.printf("%.1f\n", somaAcimaDiagonal);
+        }
+    }
+}
