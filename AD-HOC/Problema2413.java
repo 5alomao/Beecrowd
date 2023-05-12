c88c56a11aac13c586f4f37d7ac4335b3668550e
Salomão <101257752+5alomao@users.noreply.github.com>
2023-05-12 14:51:46 -0300

    Busca na Internet

---
+++ AD-HOC/Problema2413.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int t = sc.nextInt();
+        
+        System.out.println((t*2)*2);
+    }
+}
