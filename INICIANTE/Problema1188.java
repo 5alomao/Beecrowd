753dc55a3bf734c2c7c1b3fa70a2b16a033ea56b
Salomão <101257752+5alomao@users.noreply.github.com>
2023-05-12 13:36:59 -0300

    Área Inferior

---
+++ INICIANTE/Problema1188.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main (String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        char op = sc.next().charAt(0);
+        
+        double[][] matriz = new double[12][12];
+        double soma=0.0;
+        int cont=0;
+        for(int i=0;i<12;i++){
+            for(int j=0;j<12;j++){
+                matriz[i][j] = sc.nextDouble();
+                
+                if(j < i && i+j > 11){
+                    soma += matriz[i][j];
+                    cont++;
+                }
+            }
+        }
+        double med = soma/cont;
+        if(op == 'M')
+            System.out.printf("%.1f%n", med);
+        else if(op == 'S')
+            System.out.printf("%.1f%n", soma);
+    }
+}
