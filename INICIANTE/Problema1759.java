b109690cbbd0f402d02ed4fce4dc4522bc3d1ba1
Salomão <101257752+5alomao@users.noreply.github.com>
2023-05-12 16:24:11 -0300

    Ho Ho Ho

---
+++ INICIANTE/Problema1759.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main (String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            System.out.print("Ho");
+            if(i!=n)
+                System.out.print(" ");
+            else
+                System.out.printf("!%n");
+        }
+    }
+}
