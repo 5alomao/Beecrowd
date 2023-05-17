dcf082e52d95790af89480e6495738d3b9ba9b0e
Salomão <101257752+5alomao@users.noreply.github.com>
2023-05-17 15:09:55 -0300

    Melhor do Multiverso

---
+++ 12ª OLIP/Problema3397.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main (String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        
+        String frase;
+        
+        while(sc.hasNextLine()){
+            frase = sc.nextLine();
+            System.out.println("eu");
+        }
+    }
+}
