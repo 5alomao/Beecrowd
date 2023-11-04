62f9339b1be207f07e81bf99f5af6d839660ed12
Salomão <101257752+5alomao@users.noreply.github.com>
2023-11-03 23:39:28 -0300

    Problema1146.java
    StringBuilder string = new StringBuilder()
    string.append() - adicionar
    string.setLength() - definir tamanho

--- INICIANTE/Problema1146.java
+++ INICIANTE/Problema1146.java
@@ -1,20 +1,24 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main (String[] args) {
         
         Scanner sc = new Scanner (System.in);
+
+        int x = sc.nextInt();
+        StringBuilder saida = new StringBuilder();
+        while(x != 0) {
+            for(int i = 1; i <= x; i++){
+                if(i == x)
+                    saida.append(i);
+                else
+                    saida.append(i).append(" ");
+            }
+            System.out.println(saida.toString());
+            saida.setLength(0);
+            x = sc.nextInt();
+        } 
         
-        while(true){
-            int x = sc.nextInt();
-            if(x==0)
-                break;
-            for(int i=1;i<=x;i++){
-                System.out.printf("%d",i);
-                if(x!=i)
-                    System.out.printf(" ");
-            }   
-            System.out.println();   
-        }
-    }    
+        sc.close();
+    }
 }
